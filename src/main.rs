5dab5323580fa332c74b60767e37c4b3b09a8a2f
FloydATC <floyd@atc.no>
2022-07-30 12:02:21 +0200

    added ad-hoc custom implementation

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,22 @@ fn main() {
     let fake = AnyLogger::<FakeLogger>::new();
     fake.log("This is a test");
 
+    let custom = AnyLogger::<CustomLogger>::new();
+    custom.log("I make dis");
+
 }
 
+
+struct CustomLogger {
+}
+
+impl injection::logger::IsLogger for CustomLogger {
+
+    fn new() -> Self { 
+        Self {} 
+    }
+
+    fn log(&self, msg: &str) { 
+        println!("CUSTOM: {}", msg);
+    }
+}
